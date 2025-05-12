cd4c597af8f0728b0cb8bf665d0dddda0968aeff
upSkill24 <upskillnext24@gmail.com>
2025-05-12 23:46:44 +0100

    try build

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,23 @@
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     images: {
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'punkapi.online',
             },
+            {
+                protocol: 'https',
+                hostname: 'images.punkapi.com',
+                // pathname: '/v2/**', // Example
+            }
         ],
     },
 };
-
 
 export default nextConfig;
